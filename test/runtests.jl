2f6c7623f8a1033182a47e042a11709582a62064
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2024-03-28 19:21:14 +0100

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,25 @@
-using CFTimeSchemes
+using CFTimeSchemes: void, RungeKutta4, IVPSolver, advance!
+using UnicodePlots
+import CFTimeSchemes: tendencies!
 using Test
 
-@testset "CFTimeSchemes.jl" begin
-    # Write your tests here.
+struct Model{A}
+    z::A
+end
+tendencies!(dstate, (;z)::Model, state, _, _) = @. dstate = state*z
+
+function stability_region(Scheme)
+    x, y = range(-3, 1, 101), range(-4, 4, 201)
+    model = Model([xx+yy*1im for xx in x, yy in y])
+    z0 = one.(model.z) # complex 1
+    scheme = Scheme(model)
+    solver = IVPSolver(scheme, 1.0)
+    z1, t = advance!(void, solver, z0, 0.0, 100)
+    @info Scheme
+    display(heatmap(@. min(1.0, abs(z1))))
+    @test true
+end
+
+@testset "Stability" begin
+    stability_region(RungeKutta4)
 end
