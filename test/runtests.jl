94166741167ed78af79d53733ebd3520aca01e23
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-03-16 20:13:59 +0100

    Test Enzyme auto-diff

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,8 @@ import CFTimeSchemes: tendencies!, scratch_space, model_dstate
 
 using ForwardDiff: ForwardDiff
 using Zygote: Zygote
+using Enzyme: Enzyme
+
 using LinearAlgebra: dot
 using UnicodePlots
 using Test
@@ -12,22 +14,16 @@ using Test
 import Zygote.ChainRulesCore: rrule
 
 # custom rule for muladd, currently flawed in Zygote
-@inline rrule(::typeof(muladd), a,b,c) = muladd(a,b,c), (x)->(b*x, a*x, x)
+# @inline rrule(::typeof(muladd), a,b,c) = muladd(a,b,c), (x)->(b*x, a*x, x)
+
+#========= simple model dx/dt = x*z =========#
 
 struct Model{A}
     z::A
 end
 tendencies!(dstate, _, m::Model, state, _) = (@. dstate = state*m.z), nothing
 tendencies!(dstate, _, m::Model, state, _, tau) = (@. dstate = state*m.z/(1-tau*m.z)), nothing
-#=
-function tendencies!(k, l, _, m::Model, state, _, tau) 
-#    l, _ = tendencies!(l, nothing, m, state, nothing, tau)
-#    k = @. k = zero(l)
-    k, _ = tendencies!(k, nothing, m, state, nothing)
-    l = @. l = zero(k)
-    return k, l, nothing
-end
-=#
+
 function tendencies!(k, l, _, m::Model, state, _, tau)
     # tendencies are 1/2 explicit, 1/2 implicit
     k = @. k = m.z*state / (2-m.z*tau)
@@ -42,6 +38,51 @@ function make_model(Scheme)
     scheme = Scheme(model)
     return model, scheme, z0
 end
+
+#============ same but with a named tuple and scratch space to challenge AD ===========#
+
+struct Model2{A}
+    z::A
+end
+function tendencies!(dstate, scratch, m::Model2, state, _) # fully explicit
+    (; a, b) = state
+    da, db, z = dstate.a, dstate.b, m.z
+    scratch = @. scratch = z
+    da = @. da = a * scratch
+    db = @. db = b * scratch
+    return (a=da, b=db), scratch
+end
+
+function tendencies!(dstate, scratch, m::Model2, state, _, tau)  # fully implicit
+    (; a, b) = state
+    da, db, z = dstate.a, dstate.b, m.z
+    scratch = @. scratch = z/(1-tau*z)
+    da = @. da = a * scratch
+    db = @. db = b * scratch
+    return (a=da, b=db), scratch
+end
+
+function tendencies!(k, l, scratch, m::Model2, state, _, tau) # IMEX
+    # tendencies are 1/2 explicit, 1/2 implicit
+    (; a, b) = state
+    ka, kb, la, lb, z = k.a, k.b, l.a, l.b, m.z
+    scratch = @. scratch = z/(2-tau*z)
+    ka = @. ka = a * scratch
+    kb = @. kb = b * scratch
+    la = @. la = a * scratch
+    lb = @. lb = b * scratch
+    return (a=ka, b=kb), (a=la, b=lb), scratch
+end
+
+function make_model2(Scheme)
+    x, y = range(-3, 0.5, 101), range(-4.5, 4.5, 201)
+    model = Model2([xx+yy*1im for xx in x, yy in y])
+    z0 = one.(model.z) # complex 1
+    scheme = Scheme(model)
+    return model, scheme, (a = z0, b=copy(z0))
+end
+
+#==========================================================================================#
 
 accuracy(::BackwardEuler) = 1e-5 # first-order scheme
 accuracy(::Union{KinnmarkGray{2,5}, Midpoint, TRBDF2, ARK_TRBDF2}) = 1e-9 # second-order scheme
@@ -65,45 +106,52 @@ function stability_region(Scheme)
 end
 
 function autodiff(Scheme)
-    model, scheme, z0 = make_model(Scheme)
+    model, scheme, z0 = make_model2(Scheme)
     solver = IVPSolver(scheme, 1.0)
 
     # first, check that non-mutating variant supports ForwardDiff
     function loss(x)
-        zx = x*z0
+        zx = (a=x.*z0.a, b=x.*z0.b)
         z1, t = advance!(void, solver, zx, 0.0, 10)
-        sum(abs2, z1)
+        sum(abs2, z1.a)
     end
     g_ssa = ForwardDiff.derivative(loss, 1.0)
     @test true
 
     # now check mutating variant
     function loss_mutating(x)
-        zx = x*z0
-        z1 = similar(zx)
+        zx = (a=x.*z0.a, b=x.*z0.b)
+        z1 = map(similar, zx)
         solver! = IVPSolver(scheme, 1.0, zx, 0.0)
         advance!(z1, solver!, zx, 0.0, 10)
-        sum(abs2, z1)
+        sum(abs2, z1.a)
     end
     g_mut = ForwardDiff.derivative(loss_mutating, 1.0)
 
     # and Zygote
     function loss_zyg(z)
         z1, t = advance!(void, solver, z, 0.0, 10)
-        sum(abs2, z1)
+        sum(abs2, z1.a)
     end
     _, pb = Zygote.pullback(loss_zyg, z0)
-    g_zyg = dot(z0, pb(1)[1])
+    g_zyg = dotprod(z0, pb(1.0)[1])
+
+    # and Enzyme
+    Dup(x) = Enzyme.Duplicated(x, Enzyme.make_zero(x))
+    (g_enz,) = Enzyme.gradient(Enzyme.Reverse, Dup(loss_mutating), 1.0)
 
     # check mutual consistency
-    @info Scheme g_ssa g_mut g_zyg
-    @test (g_ssa ≈ g_mut) & (g_ssa ≈ g_zyg)
+    @info Scheme g_ssa g_mut g_zyg g_enz
+    @test (g_ssa ≈ g_mut) & (g_ssa ≈ g_zyg) & (g_ssa ≈ g_enz)
 end
+
+dotprod(a,b) = dot(a,b)
+dotprod(a::Array, ::Nothing) = zero(eltype(a))
+dotprod(a::NamedTuple, b::NamedTuple) = mapreduce(dotprod, +, a, b)
 
 Schemes = [RungeKutta4, KinnmarkGray{2,5}, KinnmarkGray{3,5}, 
             BackwardEuler, Midpoint, TRBDF2,
             ARK_TRBDF2]
-
 @testset "Accuracy" begin
     foreach(stability_region, Schemes)
     println()
