3e91160e42eedc4f6e825bc6e09bd633adff9180
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-03-17 23:44:10 +0100

    Cleanup + v0.3.4

--- src/julia/update.jl
+++ src/julia/update.jl
@@ -45,8 +45,8 @@ end
     return :( NamedTuple{$names}($code))
 end
 
-update_array(::Val{1}, x, u, (a,),      (ka,))         = @. x = muladd(a, ka, u)
 # we sacrifice one muladd to group the increments (see complex arrays), potentially more accurate
+update_array(::Val{1}, x, u, (a,),      (ka,))         = @. x = muladd(a, ka, u)
 update_array(::Val{2}, x, u, (a,b),     (ka,kb))       = @. x = muladd(b, kb, a*ka) + u
 update_array(::Val{3}, x, u, (a,b,c,),  (ka,kb,kc))    = @. x = muladd(c, kc, muladd(b, kb, a*ka)) + u
 update_array(::Val{4}, x, u, (a,b,c,d), (ka,kb,kc,kd)) = @. x = muladd(d, kd, muladd(c, kc, muladd(b, kb, a*ka))) + u
@@ -56,78 +56,6 @@ update_carray(::Val{1}, x, u, (a,),      (ka,))         = @. x = a*ka + u
 update_carray(::Val{2}, x, u, (a,b),     (ka,kb))       = @. x = (b*kb + a*ka) + u
 update_carray(::Val{3}, x, u, (a,b,c,),  (ka,kb,kc))    = @. x = (c*kc + b*kb + a*ka) + u
 update_carray(::Val{4}, x, u, (a,b,c,d), (ka,kb,kc,kd)) = @. x = (d*kd + c*kc + b*kb + a*ka) + u
-
-#=
-
-update!(x, mgr, u::S, a::F, ka::S) where {F, S} = 
-    LinUp(mgr, (a,))(x,u,(ka,))
-update!(x, mgr, u::S, a::F, ka, b::F, kb) where {F, S} =
-    LinUp(mgr, (a,b))(x,u,(ka,kb))::S
-update!(x, mgr, u::S, a::F, ka, b::F, kb, c::F, kc) where {F, S} = 
-    LinUp(mgr, (a,b,c))(x,u,(ka,kb,kc))::S
-update!(x, mgr, u::S, a::F, ka, b::F, kb, c::F, kc, d::F, kd) where {F, S} = 
-    LinUp(mgr, (a,b,c,d))(x,u,(ka,kb,kc,kd))::S
-
-style(x) = x
-style(::NamedTuple{names}) where names = NamedStyle{names}()
-style(::AbstractArray{T,N}) where {T,N} = ArrayStyle{N}()
-
-struct LinUp{F,N,Manager} # linear update with coefs=(a,b, ...)
-    mgr::Manager # nothing for plain broadcast, see `manage`
-    coefs::NTuple{N,F}
-end
-
-# we define these "styles" in order to be able
-# to dispatch on a tuple of named tuples with the same names but different types
-# or tuple of arrays with same rank but different types
-struct NamedStyle{names} end
-struct ArrayStyle{N} end
-
-# generic LinUp: add style info to facilitate dispatch
-(up::LinUp)(x, u::S, ks::Tuple) where S = up(x, style(u), map(style, ks), u, ks)::S
-
-# LinUp on named tuples
-function (up::LinUp{F,N})(x, s::S, ::NTuple{N,S}, u, ka) where {F, N, S<:NamedStyle}
-    return map(up, svoid(x,u), u, transp(s, ka))
-end
-
-# LinUp on tuples
-function (up::LinUp{F,N})(x, u::T, ka::NTuple{N,T}) where {F, N, T<:Tuple}
-    return map(up, svoid(x,u), u, transpose_tuple(ka))
-end
-
-svoid(::Void, u) = map(uu->void, u)
-svoid(x, u) = x
-
-@inline function transp(::NamedStyle{names}, ntup) where {names}
-    M = length(names) # compile-time constant
-    getindexer(i) = coll->coll[i]
-    t = ntuple(
-        let nt=ntup
-            i->map(getindexer(i), nt)
-        end,
-        Val{M}())
-    return NamedTuple{names}(t)
-end
-
-@inline function transpose_tuple(ntup)
-    M = length(ntup[1]) # compile-time constant
-    getindexer(i) = coll->coll[i]
-    return ntuple(
-        let nt=ntup
-            i->map(getindexer(i), nt)
-        end,
-        Val{M}())
-end
-
-# LinUp on arrays
-(up::LinUp{F,N})(x, ::S, ::NTuple{N, S}, u::A, ks) where {A, F, N, S<:ArrayStyle} = 
-    update_array(Val(N), manage(x, up.mgr), u, up.coefs, ks)::A
-(up::LinUp{F,N})(x, ::S, ::NTuple{N,S}, u::A, ks) where {N, F, A<:Array{<:Complex}, S<:ArrayStyle} = 
-    update_carray(Val(N), manage(x, up.mgr), u, up.coefs, ks) :: A
-=#
-
-
 
 """
     managed_x = manage(x, mgr)
