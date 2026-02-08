a9384c31186e3b08c57b22433fe5684756da47be
Thomas Dubos <dubos@lmd.polytechnique.fr>
2026-02-08 11:09:24 +0100

    Fix `scratch_space` : in `tendencies! `, `dstate` and `scratch` should be both `void`, or neither. (#23)

--- src/julia/imex.jl
+++ src/julia/imex.jl
@@ -34,8 +34,8 @@ max_time_step(::ARK_TRBDF2, tau) = tau
 
 function scratch_space((; model)::ARK_TRBDF2, u0, t0::Number)
     k0, l0, scratch = tendencies!(void, void, void, model, u0, t0, zero(t0)) 
-    k1, l1, _ = tendencies!(void, void, scratch, model, u0, t0, zero(t0)) 
-    k2, l2, _ = tendencies!(void, void, scratch, model, u0, t0, zero(t0)) 
+    k1, l1, _ = tendencies!(void, void, void, model, u0, t0, zero(t0)) 
+    k2, l2, _ = tendencies!(void, void, void, model, u0, t0, zero(t0)) 
     return (; scratch, k0, k1, k2, l0, l1, l2)
 end
 
